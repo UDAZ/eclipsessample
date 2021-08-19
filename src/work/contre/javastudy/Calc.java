0d044c9531d2ff767cd046149e85276655abe5b9
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-08-19 15:06:44 +0900

    add 41.式を使ってみよう

---
+++ src/work/contre/javastudy/Calc.java
@@ -0,0 +1,16 @@
+package work.contre.javastudy;
+
+public class Calc {
+
+	public static void main(String[] args) {
+		// 算術演算子 + - * / %
+		int a = 15;
+		int b = 4;
+		System.out.println(a+b);//足し算
+		System.out.println(a-b);//引き算
+		System.out.println(a*b);//かけ算
+		System.out.println(a/b);//割り算
+		System.out.println(a%b);//余り
+	}
+
+}
