567aa8d37015c2e06008abaf03c7f32700b4aa17
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-08-19 12:25:15 +0900

    add 34. 新しいメソッドを追加してみよう

--- src/work/contre/javastudy/StaffInfo.java
+++ src/work/contre/javastudy/StaffInfo.java
@@ -4,6 +4,10 @@ class Staff {
 	String name;
 	int staffid;
 	String email;
+	
+	public void sayhello(){
+		System.out.println("Hello " + this.name);
+	}
 }
 
 public class StaffInfo {
@@ -12,7 +16,8 @@ public class StaffInfo {
 		// TODO 自動生成されたメソッド・スタブ
 		Staff yamada = new Staff();
 		yamada.name = "Taro Yamada";
-		System.out.println(yamada.name);
+		yamada.sayhello();
+		// System.out.println(yamada.name);
 	}
 
 }
