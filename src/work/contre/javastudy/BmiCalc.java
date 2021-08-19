048eb81a52b078af8d360ebeb2d3ac8c3a13a70d
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-08-19 15:42:37 +0900

    add 44.代入演算子を使ってみよう

--- src/work/contre/javastudy/BmiCalc.java
+++ src/work/contre/javastudy/BmiCalc.java
@@ -3,13 +3,14 @@ package work.contre.javastudy;
 public class BmiCalc {
 /* BMIを計算するプログラム
  * BMI = 体重 / (身長 *  身長)
- * 体重 kg 、 身長 m
+ * 体重 kg 、 身長 cm
  * 
  */
 	public static void main(String[] args) {
 		double weight,height,bmi;
 		weight = 71;
-		height = 1.8;
+		height = 180;
+		height /= 100; // 代入演算子 X = X /Y などの状況の際に使える
 		bmi = weight / (height * height);
 		
 		System.out.println(bmi);
