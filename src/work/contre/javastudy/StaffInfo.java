5956fe1c87c596baddd1a1f1ae8df942e809e22d
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-08-19 12:11:05 +0900

    add comment out

--- src/work/contre/javastudy/StaffInfo.java
+++ src/work/contre/javastudy/StaffInfo.java
@@ -7,7 +7,7 @@ class Staff {
 }
 
 public class StaffInfo {
-
+// ↓のメソッドの意味。他のクラスから呼び出せてクラスから直接呼び出せて返り値をもたないargsという配列型の引数を持つ
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		Staff yamada = new Staff();
