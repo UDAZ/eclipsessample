d21e90ecc61e4b59f64e415d76a488b0cb172af1
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-08-21 17:42:03 +0900

    add 57. コンストラクタを定義してみよう

--- src/work/contre/javastudy/StaffInfo.java
+++ src/work/contre/javastudy/StaffInfo.java
@@ -8,23 +8,29 @@ class Staff {
 	public void sayhello(){
 		System.out.println("Hello " + this.name);
 	}
+
+	public Staff(String name, int staffid, String email) {
+		super();
+		this.name = name;
+		this.staffid = staffid;
+		this.email = email;
+	}
 }
 
 public class StaffInfo {
 // ↓のメソッドの意味。他のクラスから呼び出せてクラスから直接呼び出せて返り値をもたないargsという配列型の引数を持つ
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		Staff yamada = new Staff();
-		yamada.name = "Taro Yamada";
+		Staff yamada = new Staff("Taro Yamada",123,"yamda@com");
+		//yamada.name = "Taro Yamada";
 		yamada.sayhello();
 		// System.out.println(yamada.name);
 	}
 
 }
 
-/*
- * ソースコードを書く時のルール
- * １．文字列以外は、半角英数字で書く
- * ２．大文字と小文字は区別される
- * ３．文の最後はセミコロンで終わる
+/*インスタンス生成時に初期値をセットするコンストラクタを生成
+ */
+/* ソースのメニュータブ
+ * フィールドを使用してコンストラクターを作成
  */
